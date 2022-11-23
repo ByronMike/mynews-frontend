9e5bee8d4bf0fb9b743590dcb93ef54fd52f74c1
Michael <auger.michaell@gmail.com>
2022-11-23 20:13:34 +0100

    commit

--- components/Article.js
+++ components/Article.js
@@ -19,7 +19,7 @@ function Article(props) {
       return;
     }
 
-    fetch(`http://localhost:3000/users/canBookmark/${user.token}`)
+    fetch(`https://mynews-backend.vercel.app/users/canBookmark/${user.token}`)
       .then((response) => response.json())
       .then((data) => {
         console.log(props.isBookmarked);
